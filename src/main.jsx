236a6bfe29613850b3b57d84e9be403aec12cafc
Tom12039811 <tom.volaku@gmail.com>
2026-05-29 23:38:16 +0200

    V6 back

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import LexiCard from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <LexiCard />
-  </StrictMode>,
-)
